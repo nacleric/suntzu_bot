6093dba43a3e3ddfbe3aedcd0597a46f1665910c
Eric Ng <naclericny@gmail.com>
2018-02-21 03:22:28 -0500

    added a quote to test bot

--- suntzu.py
+++ suntzu.py
@@ -10,4 +10,9 @@ async def on_ready():
   print(client.user.id)
   print('---')
 
+@client.event
+async def on_message(message):
+  if message.content.startswith('!quote'):
+     await client.send_message(message.channel, 'The Art of War')
 
+client.run('NDE1NjU5NzAwNzU2MDIxMjY4.DW6ujw.X83tKLyPJ3MJ51XIMTD1jDhzFec')
