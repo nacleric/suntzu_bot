56e98eded33f6a2e1e933cef31d86b0a12bf1297
Eric Ng <naclericny@gmail.com>
2018-02-21 01:29:02 -0500

    initialized repo

---
+++ suntzu.py
@@ -0,0 +1,13 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Logged in as')
+  print(client.user.name)
+  print(client.user.id)
+  print('---')
+
+
